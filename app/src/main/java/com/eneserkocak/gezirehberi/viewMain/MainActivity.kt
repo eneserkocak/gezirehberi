f2c65b3e15613b81f2451e44a85e56743f730b16
yucelkozan <yucelkozan3571@gmail.com>
2024-09-04 22:12:00 +0300

    gson proguard added

--- app/src/main/java/com/eneserkocak/gezirehberi/viewMain/MainActivity.kt
+++ app/src/main/java/com/eneserkocak/gezirehberi/viewMain/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
